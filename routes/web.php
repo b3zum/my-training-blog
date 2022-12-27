dd1b97c2d6e97fe69e8f1f0f7eadb686b1f923c2
b3zum <b3.zum@yandex.ru>
2022-12-27 17:56:14 +0400

    read(index) and create

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\Category\CategoryCreateController;
 use App\Http\Controllers\Admin\Category\CategoryIndexController;
+use App\Http\Controllers\Admin\Category\CategoryStoreController;
 use App\Http\Controllers\Admin\Main\AdminIndexController;
 use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
@@ -30,6 +31,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
         Route::get('/',  CategoryIndexController::class)->name('admin.category.index');
         Route::get('/create',  CategoryCreateController::class)->name('admin.category.create');
+        Route::post('/',  CategoryStoreController::class)->name('admin.category.store');
     });
 });
 Auth::routes();
